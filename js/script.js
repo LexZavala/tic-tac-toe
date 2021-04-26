346e151af68c79e8b3a2d16fce6a9edae6638cbe
Lex Zavala <lex.zavala7@gmail.com>
2021-04-25 22:19:09 -0500

    Added functions to the controller to handle the cell clicks and track the index of the cells

--- js/script.js
+++ js/script.js
@@ -7,8 +7,19 @@ use it later on
 const statusDisplay = document.querySelector('.game--status');
 /*
 Here we declare some variables that we will use to track the
-game state throught the game.
+game state throughout the game.
 */
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 /*
 We will use gameActive to pause the game in case of an end scenario
 */
@@ -36,7 +47,8 @@ We set the inital message to let the players know whose turn it is
 */
 statusDisplay.innerHTML = currentPlayerTurn();
 function handleCellPlayed() {
-
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
 }
 function handlePlayerChange() {
 
@@ -45,8 +57,15 @@ function handleResultValidation() {
 
 }
 function handleCellClick() {
-
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
 }
+
 function handleRestartGame() {
 
 }
